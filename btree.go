fae2b5b418497cac870e99553646144a7f623f47
rob <robgrovesemail@gmail.com>
2017-09-03 08:28:50 -0700

    Implement insertion for BTree
    Insertion is a pretty involved process. It makes use of the following
    functionality added with this commit:
    1. Methods find() and insert() for items
       The items array is a sorted array. These methods ensure that it
       remains sorted as new items are added.
       The find() also helps to keep children arrays sorted, as each index
       in items maps to a corresponding index in children.
    
    2. Method sort() for BTree
       This method handles most of the work for insertion.
       The method recursively does the following:
       First, it inserts an item into a given node's items array (initially
       always a leaf).
       It then performs the following checks:
       A. If number of items in node is less than order of tree, the
          function is done.
       B. Else, split the node in half around a median item value.
          B1. If the current node is root, create a new root.
              Assign the median value to root's items, and assign the new
              left and right node to new root's children.
              Finally, reassign the trees root.
              The function is then finished.
          B2. Else, insert pointers to the left and right nodes into the
              current node's children array.
              Then call the function again, but this time with node's parent
              and the median value.
              This process continues until either A or B1 happen.
       In this way, split() ensures that the tree remains balanced.
    
    3. Method Insert() for BTree
       This is the exported method that users can call to insert a value.
       At the moment, Insert() ignores duplicate values. Unfortunately, it
       does so in a very clunky way. This is something I plan to address
       later.
    
    4. Expanded function newNode()
       In order to assist creating left and right nodes in split(), I've
       changed newNode() so that it can accept items and children to
       populate those fields.
    
    5. Function walk() for *node
       This function is used to visualize a subtree, and is particularly
       useful during testing. The function was hacked together during
       testing, and is something I'll likely clean up later. I may also make
       it exported.
    
    6. TestInsert and testing helpers
       TestInsert consists of a series of tests, most of which involve
       inserting a reasonably large number of items into a Tree and testing
       that the tree satisfies all the B-Tree invariants.
       To verify the invariants hold, I've added the test helper function
       isValidBTree(). The function calls a list of other functions, each
       checking that 1 of the invariants hold. If any fail, the function
       returns false.
       This function(s) will be a central part of all the BTree tests I
       write, as they test properties shared by all BTrees.

--- btree.go
+++ btree.go
@@ -1,5 +1,10 @@
 package btree
 
+import (
+	"fmt"
+	"sort"
+)
+
 //=============================================================================
 //= Types
 //=============================================================================
@@ -29,13 +34,92 @@ type node struct {
 //= Methods
 //=============================================================================
 
-func split(items items) (left, right items, sep Item) {
-	return
+// find returns the index of the item in items.
+// If item does not exist in items, return where it would be located
+// (where 0 <= index <= len(array)).
+func (its *items) find(it Item) int {
+	return sort.Search(len(*its), func(i int) bool { return it.Less((*its)[i]) })
+}
+
+// insert inserts the item into items.
+// It returns the index where the item was inserted.
+func (its *items) insert(it Item) int {
+	i := its.find(it)
+	*its = append(*its, nil)
+	copy((*its)[i+1:], (*its)[i:])
+	(*its)[i] = it
+	return i
+}
+
+// split inserts an item into a particular node.
+// After inserting the item into the node's 'items' field, the function
+// performs a series of checks / operations to ensure that the B-Tree remains
+// balanced and its invariants hold.
+// Note that this process can be recursive.
+func (b *BTree) split(node *node, item Item) {
+	node.items.insert(item)
+	if len(node.items) < b.order {
+		return
+	}
+
+	mid := len(node.items) / 2
+	midItem := node.items[mid]
+	var lc children
+	var rc children
+	if len(node.children) > 0 {
+		lc = append(lc, node.children[:mid+1]...)
+		rc = append(rc, node.children[mid+1:]...)
+	}
+	li := append(items(nil), node.items[:mid]...)
+	ri := append(items(nil), node.items[mid+1:]...)
+	leftNode := newNode(b.order, li, lc, node.parent)
+	rightNode := newNode(b.order, ri, rc, node.parent)
+	for _, c := range leftNode.children {
+		c.parent = leftNode
+	}
+	for _, c := range rightNode.children {
+		c.parent = rightNode
+	}
+
+	// If at root, create new root
+	if node.parent == nil {
+		newRoot := newNode(b.order, items{midItem}, children{leftNode, rightNode}, nil)
+		leftNode.parent = newRoot
+		rightNode.parent = newRoot
+		b.root = newRoot
+		return
+	}
+
+	// Overwrite reference to existing parent at index i.
+	i := node.parent.items.find(item)
+	node.parent.children = append(node.parent.children, nil)
+	copy(node.parent.children[i+1:], node.parent.children[i:])
+	node.parent.children[i] = leftNode
+	node.parent.children[i+1] = rightNode
+	b.split(node.parent, midItem)
 }
 
 // Insert inserts a new item into the BTree.
+// TODO: Handle case where item is equal to item alreay in tree
+// Try during for loop below.
 func (b *BTree) Insert(item Item) {
-
+	curr := b.root
+	i := curr.items.find(item)
+	if i-1 >= 0 && i-1 < len(curr.items) && !(item.Less(curr.items[i-1]) || curr.items[i-1].Less(item)) {
+		return
+	}
+	for len(curr.children) > 0 {
+		i := curr.items.find(item)
+		// If equal item is found, return without inserting.
+		// NOTE: find() returns the smallest index i such that
+		// items[i] < item.
+		// So if item is a duplicate, exisitng value will be at i-1.
+		if i-1 >= 0 && i-1 < len(curr.items) && !(item.Less(curr.items[i-1]) || curr.items[i-1].Less(item)) {
+			return
+		}
+		curr = curr.children[i]
+	}
+	b.split(curr, item)
 }
 
 // Delete deletes an item from the Btree.
@@ -62,10 +146,16 @@ func (b *BTree) Bulkload(items items) {
 // the BTree's order.
 // Note that by definition a non-leaf node with k children contains k-1 keys.
 // Hence the capacity of items is 1 less than that of children.
-func newNode(order int, parent *node) *node {
+func newNode(order int, i items, c children, parent *node) *node {
+	if i == nil {
+		i = make(items, 0, order-1)
+	}
+	if c == nil {
+		c = make(children, 0, order)
+	}
 	return &node{
-		items:    make(items, 0, order-1),
-		children: make(children, 0, order),
+		items:    i,
+		children: c,
 		parent:   parent,
 	}
 }
@@ -74,6 +164,44 @@ func newNode(order int, parent *node) *node {
 func NewBTree(order int) *BTree {
 	return &BTree{
 		order: order,
-		root:  newNode(order, nil),
+		root:  newNode(order, nil, nil, nil),
 	}
+}
+
+type walker struct {
+	n      *node
+	height int
+}
+
+func walk(root *node) {
+	total := 0
+	q := []walker{{root, 0}}
+	if len(q) > 0 {
+		total += len(root.items)
+	}
+	currHeight := 0
+	var currParent *node
+	for len(q) > 0 {
+		curr := q[0]
+		if curr.height != currHeight {
+			fmt.Println()
+			fmt.Println()
+			currHeight = curr.height
+		}
+		//fmt.Printf("(p: %p, v: %v, c: %v) ", curr.n, curr.n.items, curr.n.children)
+		if curr.n.parent != currParent {
+			fmt.Printf("\n| ")
+			currParent = curr.n.parent
+		}
+		fmt.Printf("(s: %p, p: %p, v: %v, nc: %d) ", curr.n, curr.n.parent, curr.n.items, len(curr.n.children))
+		q = q[1:]
+		for _, c := range curr.n.children {
+			if c != nil {
+				w := walker{c, curr.height + 1}
+				q = append(q, w)
+				total += len(c.items)
+			}
+		}
+	}
+	fmt.Printf("total nodes: %d\n", total)
 }
