51e91c8a39cdd58d538811240868bdf56477c10a
rob <robgrovesemail@gmail.com>
2017-09-02 18:21:06 -0700

    Add initial type, methods, funcs, tests for BTree
    This commit describes a framework for implementing the B-Tree data
    structure. In particular, it contains:
    1. Type and Interface Definitions
       A. A BTree represents a B-Tree
       B. A node represents a node in the B-Tree
       C. An Item represents a value which can be compared to another Item.
       NOTE: Instead of node's containing separate sets of keys and values,
       I've decided to follow Google's example and instead combine the two
       into a single array of comparable Items.
       This allows the tree to be more generic.
    2. Methods
       Added function headers for basic B-Tree operations (Insert, Delete,
       Search, and Bulkload).
       Bulkload exists to give users an easy way to transform an existing
       array of Items into a B-Tree.
       I'll be handling these in the next.
    3. Functions
       Added functions for initializing BTree and its nodes.
    4. Tests
       Added a series of helper test functions to ensure that a BTree
       satisfies the definition of a B-Tree.
       The plan is to have this function be called during normal tests to
       ensure that, in addition to the quality being tested, the structure
       of the underlying tree remains intact.
       NOTE: These functions need to be able to access unexported values
       from the btree package.

--- btree.go
+++ btree.go
@@ -1,1 +1,73 @@
 package btree
+
+//=============================================================================
+//= Types
+//=============================================================================
+
+// A BTree represents a B-Tree.
+type BTree struct {
+	order int   // Maximum number of children each node can have.
+	root  *node // Root node of BTree.
+}
+
+// An Item is an element which can be compared to another Item.
+type Item interface {
+	Less(other Item) bool
+}
+
+type items []Item
+
+type children []*node
+
+type node struct {
+	items    items
+	children children
+}
+
+//=============================================================================
+//= Methods
+//=============================================================================
+
+// Insert inserts a new item into the BTree.
+func (b *BTree) Insert(item Item) {
+
+}
+
+// Delete deletes an item from the Btree.
+func (b *BTree) Delete(item Item) {
+
+}
+
+// Search searches for an item in the Btree.
+func (b *BTree) Search(item Item) {
+
+}
+
+// Bulkload initializes a BTree using an array of items.
+func (b *BTree) Bulkload(items items) {
+
+}
+
+//=============================================================================
+//= Functions
+//=============================================================================
+
+// newNode returns a new node.
+// The node's fields are initalized with 0 length and capacity determined by
+// the BTree's order.
+// Note that by definition a non-leaf node with k children contains k-1 keys.
+// Hence the capacity of items is 1 less than that of children.
+func newNode(order int) *node {
+	return &node{
+		items:    make(items, 0, order-1),
+		children: make(children, 0, order),
+	}
+}
+
+// NewBTree returns a new BTree.
+func NewBTree(order int) *BTree {
+	return &BTree{
+		order: order,
+		root:  newNode(order),
+	}
+}
--- btree_test.go
+++ btree_test.go
@@ -1,26 +1,130 @@
 package btree
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func walk(tree *Btree) {
+func walk(tree *BTree) {
 
 }
 
-// checkBtreeInvariants checks that given tree satisfies the definition of a
+func atMostChildren(curr *node, max int) bool {
+	if len(curr.children) > max {
+		return false
+	}
+	for _, c := range curr.children {
+		if !atMostChildren(c, max) {
+			return false
+		}
+	}
+	return true
+}
+
+func atLeastChildren(curr *node, min int) bool {
+	if len(curr.children) == 0 {
+		return true
+	}
+
+	if len(curr.children) < min {
+		return false
+	}
+	for _, c := range curr.children {
+		if !atLeastChildren(c, min) {
+			return false
+		}
+	}
+	return true
+
+}
+
+func atLeastChildrenRoot(root *node) bool {
+	if len(root.children) == 0 {
+		return true
+	}
+	if len(root.children) >= 2 {
+		return true
+	}
+	return false
+}
+
+func rightNumKeys(curr *node) bool {
+	if len(curr.children) == 0 {
+		return true
+	}
+	if len(curr.children)-len(curr.items) != 1 {
+		return false
+	}
+	for _, c := range curr.children {
+		if !rightNumKeys(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func allLeavesSameDepthRecurse(curr *node, currDepth, wantDepth int) bool {
+	if len(curr.children) == 0 {
+		if currDepth != wantDepth {
+			return false
+		}
+	}
+	for _, c := range curr.children {
+		if !allLeavesSameDepthRecurse(c, currDepth+1, wantDepth) {
+			return false
+		}
+	}
+	return true
+}
+
+func allLeavesSameDepth(root *node) bool {
+	expectedDepth := 0
+	curr := root
+	for len(curr.children) > 0 {
+		expectedDepth++
+		curr = curr.children[0]
+	}
+
+	return allLeavesSameDepthRecurse(root, 0, expectedDepth)
+}
+
+// isValidBTree checks that given tree satisfies the definition of a
 // B-tree.
 // This function should be used at the end of each test.
-func checkBtreeInvariants(tree *Btree, order m) {
-	// For Btree of order m:
+func isValidBTree(tree *BTree) bool {
+	// For BTree of order m:
 	// 1. Every node has at most m children
+	if !atMostChildren(tree.root, tree.order) {
+		fmt.Printf("Every node must have at most order m children\n")
+		return false
+	}
 	// 2. Every non-leaf node (except root) has at least [m/2] children
+	if !atLeastChildren(tree.root, tree.order/2) {
+		fmt.Printf("Every non-leaf node must have at least order m / 2 children\n")
+		return false
+	}
 	// 3. The root has at least two children if it is not a leaf
+	if !atLeastChildrenRoot(tree.root) {
+		fmt.Printf("Every non-leaf root must have at least 2 children\n")
+		return false
+	}
 	// 4. A non-leaf node with k children contains k-1 keys
+	if !rightNumKeys(tree.root) {
+		fmt.Printf("Every non-leaf node with k children must have k-1 keys\n")
+		return false
+	}
 	// 5. All leaves appear in the same level
+	if !allLeavesSameDepth(tree.root) {
+		fmt.Printf("All leaves must have same depth\n")
+		return false
+	}
 
+	return true
 }
 
 func TestInsert(t *testing.T) {
-
+	b := NewBTree(1)
+	fmt.Printf("%v\n", isValidBTree(b))
 }
 
 func TestDelete(t *testing.T) {
