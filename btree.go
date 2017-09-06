dcc789851cc0f13e08802514275aaab14c4afe22
rob <robgrovesemail@gmail.com>
2017-09-06 02:30:06 -0700

    Reorganize code and improve documentation
    To help keep things organized, I've sectioned the code into:
    1. Package statement and imports
    2. Type definitions
    3. Methods
    4. Functions
    Within each of 2,3, and 4 I've placed exported values above unexported
    ones, and within 3 I've grouped methods by receiver.
    
    In addition, I've cleaned up, reformatted, and expanded on much of the
    existing documentation.

--- btree.go
+++ btree.go
@@ -1,3 +1,4 @@
+// Package btree implements the B-Tree data structure.
 package btree
 
 import (
@@ -36,56 +37,89 @@ type node struct {
 //= Methods
 //=============================================================================
 
-// find returns the index of the item in items.
-// If item does not exist in items, return where it would be located
-// (where 0 <= index <= len(array)).
-func (its *items) find(it Item) int {
-	return sort.Search(len(*its), func(i int) bool { return it.Less((*its)[i]) })
-}
+// Insert inserts a new item into the BTree. If needed, it also rebalances the
+// tree.
+//
+// Duplicate values cannot be inserted. If the item to insert is found in the
+// tree, the method will fail silently.
+func (b *BTree) Insert(item Item) {
+	curr := b.root
+	for {
+		i := curr.items.find(item)
 
-// insert inserts the item into items.
-// It returns the index where the item was inserted.
-func (its *items) insert(it Item) int {
-	i := its.find(it)
-	*its = append(*its, nil)
-	copy((*its)[i+1:], (*its)[i:])
-	(*its)[i] = it
-	return i
-}
-
-func (its *items) truncate(newLen int) {
-	for i := newLen; i < len(*its); i++ {
-		(*its)[i] = nil
-	}
-	*its = (*its)[:newLen]
-}
-
-func (its *items) delete(index int) {
-	copy((*its)[index:], (*its)[index+1:])
-	(*its)[len(*its)-1] = nil
-	*its = (*its)[:len(*its)-1]
-}
-
-func (chi *children) delete(index int) {
-	copy((*chi)[index:], (*chi)[index+1:])
-	(*chi)[len(*chi)-1] = nil
-	*chi = (*chi)[:len(*chi)-1]
-}
-
-func (chi *children) truncate(newLen int) {
-	for i := newLen; i < len(*chi); i++ {
-		(*chi)[i] = nil
-	}
-	*chi = (*chi)[:newLen]
-}
-
-func (chi *children) indexOf(n *node) int {
-	for i, p := range *chi {
-		if p == n {
-			return i
+		if curr.items.match(item, i-1) {
+			return
+		} else if i >= len(curr.children) {
+			break
 		}
+
+		curr = curr.children[i]
 	}
-	return -1
+
+	b.split(curr, item)
+}
+
+// Delete deletes an item from the B-Tree. If needed, it also rebalances the
+// tree.
+//
+// If the item to delete does not exist in the tree, the method will fail
+// silently.
+func (b *BTree) Delete(item Item) {
+	del, i := b.search(item)
+	if i == -1 {
+		return
+	}
+	// 1. Delete the item from its container node.
+	// The container node must be either an internal node or a leaf.
+	// If it is a leaf, we can simply delete the item, as leaves do not
+	// have children that would be affected by a missing separator.
+	// If it is a container, we replace the deleted value with the maximum
+	// value of its left subtree, and delete the value from its original
+	// container node.
+	var affected *node
+	if len(del.children) == 0 {
+		del.items.delete(i)
+		affected = del
+	} else {
+		maxNode := b.max(del.children[i])
+		if len(maxNode.items) > 0 {
+			del.items[i] = maxNode.items[len(maxNode.items)-1]
+			maxNode.items.delete(len(maxNode.items) - 1)
+		}
+		affected = maxNode
+	}
+	// 2. Rebalance the tree around affected node.
+	// NOTE: Because affected is a leaf, it is only considered unbalanced
+	// if it is empty and not the root.
+	if len(affected.items) == 0 && affected.parent != nil {
+		minItems := 1
+		b.rebalance(affected, minItems)
+	}
+}
+
+// Search searches for an item in the Btree.
+//
+// If the item is found, the method returns a pointer to it.
+// Otherwise, the function returns nil and an error indicating failure.
+func (b *BTree) Search(item Item) (*Item, error) {
+	curr := b.root
+	for {
+		i := curr.items.find(item)
+		if curr.items.match(item, i-1) {
+			return &curr.items[i-1], nil
+		} else if i >= len(curr.children) {
+			break
+		}
+
+		curr = curr.children[i]
+	}
+
+	return nil, errors.New("item not found in BTree")
+}
+
+// Bulkload initializes a BTree using an array of Items.
+func (b *BTree) Bulkload(items items) {
+
 }
 
 // split inserts an item into a particular node.
@@ -125,65 +159,6 @@ func (b *BTree) split(node *node, item Item) {
 	copy(node.parent.children[i+1:], node.parent.children[i:])
 	node.parent.children[i+1] = rightNode
 	b.split(node.parent, midItem)
-}
-
-// Insert inserts a new item into the BTree.
-func (b *BTree) Insert(item Item) {
-	curr := b.root
-	// Loop continues until reaching leaf or finding equal item.
-	for {
-		i := curr.items.find(item)
-
-		if curr.items.match(item, i-1) {
-			return
-		} else if i >= len(curr.children) {
-			break
-		}
-
-		curr = curr.children[i]
-	}
-
-	b.split(curr, item)
-}
-
-// search searches for an item in the tree.
-// It returns the node containing item and the index of item in the items
-// array.
-func (b *BTree) search(item Item) (*node, int) {
-	curr := b.root
-	for {
-		i := curr.items.find(item)
-		if curr.items.match(item, i-1) {
-			return curr, i - 1
-		} else if i >= len(curr.children) {
-			return nil, -1
-		}
-		curr = curr.children[i]
-	}
-}
-
-// max returns the rightmost node of a particular subtree.
-func (b *BTree) max(root *node) *node {
-	curr := root
-	for {
-		if len(curr.children) == 0 {
-			return curr
-		}
-		curr = curr.children[len(curr.children)-1]
-	}
-}
-
-// nthChildOfParent returns the index of the child in n.parent which points to
-// n.
-// NOTE: Because find() uses binary search, we defer to it when possible.
-func (n *node) nthChildOfParent() int {
-	if n.parent == nil {
-		return -1
-	}
-	if len(n.items) == 0 {
-		return n.parent.children.indexOf(n)
-	}
-	return n.parent.items.find(n.items[0])
 }
 
 // rebalance attempts to rebalance the tree around a given node.
@@ -273,42 +248,38 @@ func (b *BTree) rebalance(n *node, minItems int) {
 	}
 }
 
-// Delete deletes an item from the Btree.
-func (b *BTree) Delete(item Item) {
-	// 1. Search for node containing element
-	del, i := b.search(item)
-	if i == -1 {
-		return
-	}
-	var affected *node
-	if len(del.children) == 0 {
-		//    A. If element is in leaf, simply delete value from node.items
-		del.items.delete(i)
-		affected = del
-	} else {
-		//    B. If element is in internal, we need to find replacement for
-		//    deleted item as separation value. To do this, we either find
-		//    largest element in left subtree or smallest element in right
-		//    subtree.
-		// Replacement is max value of items less than deleted value.
-		// Recall that for any item at index i, children[i] gives left
-		// subtree.
-		maxNode := b.max(del.children[i])
-		if len(maxNode.items) > 0 {
-			del.items[i] = maxNode.items[len(maxNode.items)-1]
-			maxNode.items.delete(len(maxNode.items) - 1)
+// search searches for an item in the tree.
+// It returns the node containing item and the index of item in the items
+// array.
+func (b *BTree) search(item Item) (*node, int) {
+	curr := b.root
+	for {
+		i := curr.items.find(item)
+		if curr.items.match(item, i-1) {
+			return curr, i - 1
+		} else if i >= len(curr.children) {
+			return nil, -1
 		}
-		affected = maxNode
+		curr = curr.children[i]
 	}
-	// 2. Replace the tree.
-	// NOTE: Because affected is a leaf, it is only considered unbalanced
-	// if it is empty and not the root.
-	if len(affected.items) == 0 && affected.parent != nil {
-		// As affected is a leaf node, its only requirement is that it
-		// not be empty.
-		minItems := 1
-		b.rebalance(affected, minItems)
+}
+
+// max returns the rightmost node of a particular subtree.
+func (b *BTree) max(root *node) *node {
+	curr := root
+	for {
+		if len(curr.children) == 0 {
+			return curr
+		}
+		curr = curr.children[len(curr.children)-1]
 	}
+}
+
+// find returns the index of the item in items.
+// If item does not exist in items, return where it would be located
+// (where 0 <= index <= len(array)).
+func (its *items) find(it Item) int {
+	return sort.Search(len(*its), func(i int) bool { return it.Less((*its)[i]) })
 }
 
 // match checks if item and given index is equal to given item.
@@ -320,46 +291,67 @@ func (its *items) match(item Item, index int) bool {
 	return false
 }
 
-// Search searches for an item in the Btree.
-// On success, returns a pointer to the value and sets error to nil.
-// On failure, returns nil and error.
-func (b *BTree) Search(item Item) (*Item, error) {
-	curr := b.root
-	for {
-		i := curr.items.find(item)
-		if curr.items.match(item, i-1) {
-			return &curr.items[i-1], nil
-		} else if i >= len(curr.children) {
-			break
-		}
-
-		curr = curr.children[i]
-	}
-
-	return nil, errors.New("item not found in BTree")
+// insert inserts the item into items.
+// It returns the index where the item was inserted.
+func (its *items) insert(it Item) int {
+	i := its.find(it)
+	*its = append(*its, nil)
+	copy((*its)[i+1:], (*its)[i:])
+	(*its)[i] = it
+	return i
 }
 
-// Bulkload initializes a BTree using an array of items.
-func (b *BTree) Bulkload(items items) {
+func (its *items) truncate(newLen int) {
+	for i := newLen; i < len(*its); i++ {
+		(*its)[i] = nil
+	}
+	*its = (*its)[:newLen]
+}
 
+func (its *items) delete(index int) {
+	copy((*its)[index:], (*its)[index+1:])
+	(*its)[len(*its)-1] = nil
+	*its = (*its)[:len(*its)-1]
+}
+
+func (chi *children) delete(index int) {
+	copy((*chi)[index:], (*chi)[index+1:])
+	(*chi)[len(*chi)-1] = nil
+	*chi = (*chi)[:len(*chi)-1]
+}
+
+func (chi *children) truncate(newLen int) {
+	for i := newLen; i < len(*chi); i++ {
+		(*chi)[i] = nil
+	}
+	*chi = (*chi)[:newLen]
+}
+
+func (chi *children) indexOf(n *node) int {
+	for i, p := range *chi {
+		if p == n {
+			return i
+		}
+	}
+	return -1
+}
+
+// nthChildOfParent returns the index of the child in n.parent which points to
+// n.
+// NOTE: Because find() uses binary search, we defer to it when possible.
+func (n *node) nthChildOfParent() int {
+	if n.parent == nil {
+		return -1
+	}
+	if len(n.items) == 0 {
+		return n.parent.children.indexOf(n)
+	}
+	return n.parent.items.find(n.items[0])
 }
 
 //=============================================================================
 //= Functions
 //=============================================================================
-
-// newNode returns a new node.
-// The node's fields are initalized with 0 length and capacity determined by
-// the BTree's order.
-// Note that by definition a non-leaf node with k children contains k-1 keys.
-// Hence the capacity of items is 1 less than that of children.
-func newNode(order int, i items, c children, parent *node) *node {
-	return &node{
-		items:    i,
-		children: c,
-		parent:   parent,
-	}
-}
 
 // NewBTree returns a new BTree.
 func NewBTree(order int) *BTree {
@@ -369,11 +361,22 @@ func NewBTree(order int) *BTree {
 	}
 }
 
+// newNode returns a new node.
+func newNode(order int, i items, c children, parent *node) *node {
+	return &node{
+		items:    i,
+		children: c,
+		parent:   parent,
+	}
+}
+
 type walker struct {
 	n      *node
 	height int
 }
 
+// walk prints a representation of the BTree in level order.
+// Exists for testing purposes.
 func walk(root *node) {
 	total := 0
 	q := []walker{{root, 0}}
@@ -389,7 +392,6 @@ func walk(root *node) {
 			fmt.Println()
 			currHeight = curr.height
 		}
-		//fmt.Printf("(p: %p, v: %v, c: %v) ", curr.n, curr.n.items, curr.n.children)
 		if curr.n.parent != currParent {
 			fmt.Printf("\n| ")
 			currParent = curr.n.parent
